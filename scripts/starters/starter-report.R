8cab4c6a08ca3c3b740830bbb2e692153813cafd
Andriy V. Koval <koval.andrey@gmail.com>
2020-03-24 20:47:19 -0400

    adjust starters

--- scripts/starters/starter-report.R
+++ scripts/starters/starter-report.R
@@ -2,29 +2,29 @@
 rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
 cat("\f") # clear console
 
-# ---- load-sources ------------------------------------------------------------
-# Call `base::source()` on any repo file that defines functions needed below.
-source("./scripts/common-functions.R") # used in multiple reports
-source("./scripts/graphing/graph-presets.R") # fonts, colors, themes
-
-# ---- load-packages -----------------------------------------------------------
+# ---- load-packages --------------------------------------------------
 # Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
 library(magrittr) # enables piping : %>%
 library(dplyr)    # data wrangling
 library(ggplot2)  # graphs
 library(tidyr)    # data tidying
 
-# ---- declare-globals ---------------------------------------------------------
+# ---- load-sources ---------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.
+source("./scripts/common-functions.R") # used in multiple reports
+source("./scripts/graphing/graph-presets.R") # fonts, colors, themes
 
-# ---- load-data ---------------------------------------------------------------
+# ---- declare-globals ------------------------------------------------
 
-# ---- inspect-data -------------------------------------------------------------
+# ---- load-data ------------------------------------------------------
 
-# ---- tweak-data --------------------------------------------------------------
+# ---- inspect-data ---------------------------------------------------
 
-# ---- basic-table --------------------------------------------------------------
+# ---- tweak-data -----------------------------------------------------
 
-# ---- basic-graph --------------------------------------------------------------
+# ---- basic-table ----------------------------------------------------
+
+# ---- basic-graph ----------------------------------------------------
 
 # ---- publish ---------------------------------------
 path_report_1 <- "./analysis/*/report_1.Rmd"
